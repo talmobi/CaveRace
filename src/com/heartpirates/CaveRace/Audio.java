fe1dd7c0e8dcb087c066b07977ed5bad8dd2f558
unknown <talmo.christian@gmail.com>
2014-03-14 03:48:25 +0200

    added more sfx

--- src/com/heartpirates/CaveRace/Audio.java
+++ src/com/heartpirates/CaveRace/Audio.java
@@ -1,22 +1,81 @@
 package com.heartpirates.CaveRace;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.FileSystems;
 import java.util.HashMap;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
 
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.FloatControl;
 
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+
 public class Audio {
-	public static final HashMap<String, Clip> clipMap = getClips();
+	public static final String NAME = "Audio.crdata";
+	public HashMap<String, Clip> clips;
+	public static Audio i = null;
+
+	public Audio() {
+		clips = Audio.getClips();
+	}
+
+	public static Audio load() {
+		File file = FileSystems.getDefault().getPath(Audio.NAME).toFile();
+
+		if (file.exists() && file.isFile()) {
+			Kryo kryo = new Kryo();
+
+			try {
+				GZIPInputStream gin = new GZIPInputStream(new FileInputStream(
+						file));
+				Input input = new Input(gin);
+				Audio a = kryo.readObject(input, Audio.class);
+				input.close();
+				return a;
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else {
+			Audio a = new Audio();
+			return a;
+		}
+
+		return null;
+	}
+
+	public static void save(Audio audio) throws FileNotFoundException,
+			IOException {
+
+		synchronized (audio) {
+			Kryo kryo = new Kryo();
+
+			File file = FileSystems.getDefault().getPath(Audio.NAME).toFile();
+			GZIPOutputStream gout = new GZIPOutputStream(new FileOutputStream(
+					file));
+			Output output = new Output(gout);
+			kryo.writeObject(output, audio);
+			output.close();
+			System.out.println("Saved Audio.");
+		}
+	}
 
 	private static HashMap<String, Clip> getClips() {
+		System.out.println("Loading Sounds.");
+
 		HashMap<String, Clip> map = new HashMap<String, Clip>();
 		AudioInputStream ais = null;
 		Clip clip = null;
 
 		try {
-
 			ais = AudioSystem.getAudioInputStream(Audio.class.getClassLoader()
 					.getResource("snd/Blip1.wav"));
 			clip = AudioSystem.getClip();
@@ -106,16 +165,46 @@ public class Audio {
 			clip = AudioSystem.getClip();
 			clip.open(ais);
 			map.put("Error", clip);
+
+			ais = AudioSystem.getAudioInputStream(Audio.class.getClassLoader()
+					.getResource("snd/Close.wav"));
+			clip = AudioSystem.getClip();
+			clip.open(ais);
+			map.put("Close", clip);
+
+			ais = AudioSystem.getAudioInputStream(Audio.class.getClassLoader()
+					.getResource("snd/Error2.wav"));
+			clip = AudioSystem.getClip();
+			clip.open(ais);
+			map.put("Error2", clip);
+
+			ais = AudioSystem.getAudioInputStream(Audio.class.getClassLoader()
+					.getResource("snd/Open.wav"));
+			clip = AudioSystem.getClip();
+			clip.open(ais);
+			map.put("Open", clip);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
+		System.out.println("Sounds Loaded.");
 		return map;
 	}
 
 	public static void play(String name) {
+		if (Audio.i == null) {
+			Audio.i = load();
+			try {
+				Audio.save(Audio.i);
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
 		try {
-			Clip clip = clipMap.get(name);
+			Clip clip = Audio.i.clips.get(name);
 			if (clip == null)
 				return;
 
